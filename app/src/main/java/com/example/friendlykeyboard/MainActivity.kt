2f23bffb717cade010ae235fcad0de465b48b9fb
hkjeong2 <chali0330@gmail.com>
2023-03-12 19:25:03 +0900

    로그인 페이지 xml 작성

--- app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.friendlykeyboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.friendlykeyboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        changeActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
+
+    private fun changeActivity(intent : Intent){
+        startActivity(intent)
+    }
+
 }
