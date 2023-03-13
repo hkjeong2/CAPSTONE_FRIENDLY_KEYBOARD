22d754817b3cc8b35d56c724939a4754d36ec6dc
HyunJinNo <nhj9831@naver.com>
2023-03-13 13:04:47 +0900

    띄어쓰기 수정

--- app/src/main/java/com/example/friendlykeyboard/LoginActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/LoginActivity.kt
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.friendlykeyboard.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityLoginBinding
+    private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 import com.example.friendlykeyboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
