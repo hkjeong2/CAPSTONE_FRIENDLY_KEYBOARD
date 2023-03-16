36b53467abdd50edf7fca88ad655aebda7518210
hkjeong2 <chali0330@gmail.com>
2023-03-17 00:59:40 +0900

    custom keyboard extracted from open source

--- app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.friendlykeyboard
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.friendlykeyboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val settingHeader = findViewById<ConstraintLayout>(R.id.setting_header)
+        val submitButton = settingHeader.findViewById<TextView>(R.id.submit_text)
+        submitButton.visibility = View.GONE
 
 
     }
---
+++ app/src/main/java/com/example/friendlykeyboard/keyboard/KeyboardInteractionListener.kt
@@ -0,0 +1,5 @@
+package com.example.friendlykeyboard.keyboard
+
+interface KeyboardInteractionListener {
+    fun modechange(mode:Int)
+}
