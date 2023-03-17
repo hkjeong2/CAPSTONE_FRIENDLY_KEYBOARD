5d0dfc5ad390b90cfa6b5400211b2f29bedd2c27
HyunJinNo <nhj9831@naver.com>
2023-03-17 19:30:32 +0900

    execute() --> enqueue() 로 수정

--- app/src/main/java/com/example/friendlykeyboard/SignUpActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/SignUpActivity.kt
@@ -1,10 +1,7 @@
 package com.example.friendlykeyboard
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +10,9 @@ import com.example.friendlykeyboard.databinding.ActivitySignUpBinding
 import com.example.friendlykeyboard.retrofit_util.Account
 import com.example.friendlykeyboard.retrofit_util.DataModel
 import com.example.friendlykeyboard.retrofit_util.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -33,7 +33,7 @@ class SignUpActivity : AppCompatActivity() {
 
         // 아이디 중복 검사 버튼 이벤트 설정
         binding.idCheckButton.setOnClickListener {
-            idCheck = validateId()
+            validateId()
         }
         
         // 회원가입 버튼 이벤트 설정
@@ -43,55 +43,60 @@ class SignUpActivity : AppCompatActivity() {
                     val id = binding.editId.text.toString()
                     val password = binding.editPwd.text.toString()
 
-                    val result: DataModel?
                     val account = Account(id, password)
 
-                    try {
-                        val response = service.signUp(account).execute()
-                        if (response.isSuccessful) {
-                            result = response.body()
+                    service.signUp(account).enqueue(object : Callback<DataModel> {
+                        override fun onResponse(
+                            call: Call<DataModel>,
+                            response: Response<DataModel>
+                        ) {
+                            if (response.isSuccessful) {
+                                val result = response.body()
 
-                            when (result?.responseText) {
-                                "Success" -> {
-                                    Toast.makeText(
-                                        applicationContext,
-                                        "회원가입이 완료되었습니다.",
-                                        Toast.LENGTH_SHORT).show()
+                                when (result?.responseText) {
+                                    "Success" -> {
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "회원가입이 완료되었습니다.",
+                                            Toast.LENGTH_SHORT).show()
 
-                                    val intent = Intent(this, LoginActivity::class.java).apply {
-                                        putExtra("id", id)
-                                        putExtra("password", password)
+                                        val intent = Intent(this@SignUpActivity, LoginActivity::class.java).apply {
+                                            putExtra("id", id)
+                                            putExtra("password", password)
+                                        }
+                                        setResult(RESULT_OK, intent)
+                                        finish()
                                     }
-                                    setResult(RESULT_OK, intent)
-                                    finish()
+                                    else -> {
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "회원가입이 실패하였습니다.",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
                                 }
-                                else -> {
-                                    Toast.makeText(
-                                        applicationContext,
-                                        "회원가입이 실패하였습니다.",
-                                        Toast.LENGTH_SHORT).show()
-                                }
+                            } else {
+                                Log.d("SignUpActivity", response.message())
+                                // 통신이 실패한 경우
+                                Toast.makeText(
+                                    applicationContext,
+                                    "오류가 발생하였습니다.",
+                                    Toast.LENGTH_SHORT).show()
+                                setResult(RESULT_CANCELED)
+                                finish()
                             }
-                        } else {
-                            Log.d("SignUpActivity", response.message())
-                            // 통신이 실패한 경우
+                        }
+
+                        override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                            // 통신 실패 (인터넷 끊김, 예외 발생 등 시스템적인 이유
+                            t.printStackTrace()
                             Toast.makeText(
                                 applicationContext,
-                                "오류가 발생하였습니다.",
+                                "서버와의 통신이 실패하였습니다.",
                                 Toast.LENGTH_SHORT).show()
                             setResult(RESULT_CANCELED)
                             finish()
                         }
-                    } catch (e: Exception) {
-                        // 통신 실패 (인터넷 끊김, 예외 발생 등 시스템적인 이유
-                        e.printStackTrace()
-                        Toast.makeText(
-                            applicationContext,
-                            "서버와의 통신이 실패하였습니다.",
-                            Toast.LENGTH_SHORT).show()
-                        setResult(RESULT_CANCELED)
-                        finish()
-                    }
+                    })
                 }
             } else {
                 binding.editId.error = "아이디 중복 여부를 확인해주세요."
@@ -105,63 +110,66 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     // 아이디 중복 검사
-    private fun validateId(): Boolean {
+    private fun validateId() {
         val id = binding.editId.text.toString()
 
         if (id.isEmpty()) {
             binding.editId.error = "아이디를 입력해주세요."
-            return false
+            idCheck = false
+            return
         }
 
-        val result: DataModel?
         val account = Account(id, "?")
 
-        try {
-            val response = service.getAccount(account).execute()
-            if (response.isSuccessful) {
-                result = response.body()
+        service.getAccount(account).enqueue(object : Callback<DataModel> {
+            override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                if (response.isSuccessful) {
+                    val result = response.body()
 
-                when (result?.responseText) {
-                    "Available" -> {
-                        Toast.makeText(
-                            applicationContext,
-                            "사용가능한 아이디입니다.",
-                            Toast.LENGTH_SHORT).show()
-                        binding.editId.isFocusable = false
-                        binding.idCheckButton.isClickable = false
-                        return true
+                    when (result?.responseText) {
+                        "Available" -> {
+                            Toast.makeText(
+                                applicationContext,
+                                "사용가능한 아이디입니다.",
+                                Toast.LENGTH_SHORT).show()
+                            binding.editId.isFocusable = false
+                            binding.idCheckButton.isClickable = false
+                            idCheck = true
+                            return
+                        }
+                        else -> {
+                            binding.editId.error = "중복된 아이디입니다."
+                            Toast.makeText(
+                                applicationContext,
+                                "중복된 아이디입니다.",
+                                Toast.LENGTH_SHORT).show()
+                            idCheck = false
+                            return
+                        }
                     }
-                    else -> {
-                        binding.editId.error = "중복된 아이디입니다."
-                        Toast.makeText(
-                            applicationContext,
-                            "중복된 아이디입니다.",
-                            Toast.LENGTH_SHORT).show()
-                        return false
-                    }
+                } else {
+                    Log.d("SignUpActivity", response.message())
+                    // 통신이 실패한 경우
+                    Toast.makeText(
+                        applicationContext,
+                        "오류가 발생하였습니다.",
+                        Toast.LENGTH_SHORT).show()
+                    setResult(RESULT_CANCELED)
+                    finish()
                 }
-            } else {
-                Log.d("SignUpActivity", response.message())
-                // 통신이 실패한 경우
+            }
+
+            override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                // 통신 실패 (인터넷 끊김, 예외 발생 등 시스템적인 이유
+                t.printStackTrace()
                 Toast.makeText(
                     applicationContext,
-                    "오류가 발생하였습니다.",
+                    "서버와의 통신이 실패하였습니다.",
                     Toast.LENGTH_SHORT).show()
-                setResult(RESULT_CANCELED)
-                finish()
+                //setResult(RESULT_CANCELED)
+                //finish()
             }
-        } catch (e: Exception) {
-            // 통신 실패 (인터넷 끊김, 예외 발생 등 시스템적인 이유
-            e.printStackTrace()
-            Toast.makeText(
-                applicationContext,
-                "서버와의 통신이 실패하였습니다.",
-                Toast.LENGTH_SHORT).show()
-            //setResult(RESULT_CANCELED)
-            //finish()
-        }
-
-        return false
+        })
     }
 
     // 비밀번호 검사
