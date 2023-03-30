a63a6377228b69c87e228e6e890e8f528c223fc3
HyunJinNo <nhj9831@naver.com>
2023-03-30 18:49:30 +0900

    키보드 자판 색 수정 기능 구현

--- app/src/main/java/com/example/friendlykeyboard/keyboard/keyboardview/KeyboardKorean.kt
+++ app/src/main/java/com/example/friendlykeyboard/keyboard/keyboardview/KeyboardKorean.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.inputmethodservice.Keyboard
 import android.media.AudioManager
 import android.os.*
@@ -16,6 +17,7 @@ import android.view.inputmethod.InputConnection
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import com.example.friendlykeyboard.R
 import com.example.friendlykeyboard.keyboard.KeyboardInteractionListener
@@ -54,15 +56,20 @@ class KeyboardKorean constructor(var context:Context, var layoutInflater: Layout
     lateinit var thirdLine: LinearLayout
     lateinit var fourthLine: LinearLayout
 
+    // 키보드 속성 업데이트
     fun updateKeyboard(){
         val height = sharedPreferences.getInt("keyboardHeight", 150)
         val paddingLeft = sharedPreferences.getInt("keyboardPaddingLeft", 0)
         val paddingRight = sharedPreferences.getInt("keyboardPaddingRight", 0)
         val paddingBottom = sharedPreferences.getInt("keyboardPaddingBottom", 0)
         val fontColor = sharedPreferences.getInt("keyboardFontColor", 0)
+        val keyboardColor = sharedPreferences.getInt("keyboardColor", 0)
+        val keyboardBackgroundColor = sharedPreferences.getInt("keyboardBackground", 0)
 
+        // 키보드 padding 업데이트
         koreanLayout.setPadding(paddingLeft, 0, paddingRight, paddingBottom)
 
+        // 키보드 높이 업데이트
         if(context.getResources().configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
             numpadLine.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (height * 0.7).toInt())
             firstLine.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (height*0.7).toInt())
@@ -76,10 +83,15 @@ class KeyboardKorean constructor(var context:Context, var layoutInflater: Layout
             thirdLine.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, height)
             fourthLine.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, height)
         }
-
+        
+        // 키보드 자판 색, 폰트 색 업데이트
         for (button in buttons) {
             button.setTextColor(fontColor)
+            button.background.setTint(keyboardColor)
         }
+
+        // 키보드 배경색 업데이트
+        koreanLayout.setBackgroundColor(keyboardBackgroundColor)
     }
 
     fun init(){
