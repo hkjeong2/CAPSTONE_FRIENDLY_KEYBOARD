ba8d862ddb7695901401495943573aa44116127c
HyunJinNo <nhj9831@naver.com>
2023-04-05 14:36:13 +0900

    키보드 선택 화면 구현 중

---
+++ app/src/main/java/com/example/friendlykeyboard/InputMethodPickerActivity.kt
@@ -0,0 +1,44 @@
+package com.example.friendlykeyboard
+
+import android.os.Bundle
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.friendlykeyboard.databinding.ActivityInputMethodPickerBinding
+
+class InputMethodPickerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInputMethodPickerBinding
+    private lateinit var inputMethodManager: InputMethodManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInputMethodPickerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        inputMethodManager =
+            applicationContext.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        val id = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.DEFAULT_INPUT_METHOD
+        )
+
+
+        runOnUiThread {
+            binding.textView.text = buildString {
+                append(id)
+                append("\n")
+
+                if (id == "com.example.friendlykeyboard/.keyboard.KeyBoardService") {
+                    append("true")
+                } else {
+                    append("false")
+                }
+            }
+
+            inputMethodManager.showInputMethodPicker()
+        }
+
+        inputMethodManager.showInputMethodPicker()
+    }
+}
