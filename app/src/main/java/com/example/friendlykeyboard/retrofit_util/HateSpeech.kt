531784b7a983c6dc6773ce8b16f97bd5056e912d
HyunJinNo <nhj9831@naver.com>
2023-05-03 17:00:56 +0900

    서버에 혐오 표현 사용 횟수 기록

--- app/src/main/java/com/example/friendlykeyboard/retrofit_util/HateSpeech.kt
+++ app/src/main/java/com/example/friendlykeyboard/retrofit_util/HateSpeech.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 // JSON, DTO 클래스
 data class HateSpeech(
-    //@SerializedName("id") private val id: String,
+    @SerializedName("id") private val id: String,
     @SerializedName("text") private val text: String
 )
