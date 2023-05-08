e74b25947d607451a77f704423754b160e5e1579
HyunJinNo <nhj9831@naver.com>
2023-05-08 14:07:09 +0900

    차트 애니메이션 설정

--- app/src/main/java/com/example/friendlykeyboard/fragments/ChartFragment.kt
+++ app/src/main/java/com/example/friendlykeyboard/fragments/ChartFragment.kt
@@ -40,6 +40,30 @@ class ChartFragment : Fragment() {
             add(Entry(5f, 30f))
             add(Entry(6f, 25f))
             add(Entry(7f, 50f))
+            add(Entry(8f, 37f))
+            add(Entry(9f, 25f))
+            add(Entry(10f, 51f))
+            add(Entry(11f, 22f))
+            add(Entry(12f, 17f))
+            add(Entry(13f, 16f))
+            add(Entry(14f, 13f))
+            add(Entry(15f, 20f))
+            add(Entry(16f, 34f))
+            add(Entry(17f, 39f))
+            add(Entry(18f, 40f))
+            add(Entry(19f, 35f))
+            add(Entry(20f, 21f))
+            add(Entry(21f, 0f))
+            add(Entry(22f, 5f))
+            add(Entry(23f, 4f))
+            add(Entry(24f, 40f))
+            add(Entry(25f, 31f))
+            add(Entry(26f, 22f))
+            add(Entry(27f, 25f))
+            add(Entry(28f, 47f))
+            add(Entry(29f, 23f))
+            add(Entry(30f, 38f))
+            add(Entry(31f, 24f))
         }
 
         val lineDateSet = LineDataSet(mutableList, "혐오 표현 사용 횟수").apply {
@@ -47,7 +71,7 @@ class ChartFragment : Fragment() {
             color = Color.BLUE
             setCircleColor(Color.BLUE) // 데이터 원형 색 지정
             valueTextSize = 10f // 값 글자 크기
-            valueTextColor = Color.BLUE
+            valueTextColor = Color.WHITE
             lineWidth = 2f // 라인 두께
             circleRadius = 4f // 원 크기
             fillAlpha = 0 // 라인 색 투명도
@@ -72,6 +96,9 @@ class ChartFragment : Fragment() {
             position = XAxis.XAxisPosition.BOTTOM
             textColor = Color.BLUE
             granularity = 1f
+            axisMinimum = 1f
+            axisMaximum = 31f
+            labelCount = 31
             enableGridDashedLine(10f, 20f, 0f)
         }
 
@@ -86,10 +113,11 @@ class ChartFragment : Fragment() {
         binding.lineChart.axisRight.isEnabled = false
 
         with (binding.lineChart) {
+            zoom(3f, 0f, 0f, 0f)
             isDoubleTapToZoomEnabled = false
             setDrawGridBackground(false)
             description.text = ""
-            animateY(1000, Easing.EaseInCubic)
+            animateY(1000, Easing.EaseInOutCubic)
             invalidate()
         }
     }
