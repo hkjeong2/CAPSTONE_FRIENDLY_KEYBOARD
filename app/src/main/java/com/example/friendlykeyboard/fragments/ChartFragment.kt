aed98cca80dec22ecf98cb83a2ad6d8fd060fc4e
HyunJinNo <nhj9831@naver.com>
2023-05-08 14:39:17 +0900

    서버에서 혐오 표현 사용 횟수를 가져오는 함수 작성 중

--- app/src/main/java/com/example/friendlykeyboard/fragments/ChartFragment.kt
+++ app/src/main/java/com/example/friendlykeyboard/fragments/ChartFragment.kt
@@ -1,5 +1,7 @@
 package com.example.friendlykeyboard.fragments
 
+import android.app.Activity
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,14 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.friendlykeyboard.databinding.FragmentChartBinding
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class ChartFragment : Fragment() {
     private var _binding: FragmentChartBinding? = null
@@ -26,45 +26,15 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentChartBinding.inflate(layoutInflater, container, false)
+        initListeners()
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
 
-        val mutableList = mutableListOf<Entry>().apply {
-            add(Entry(1f, 1f))
-            add(Entry(2f, 2f))
-            add(Entry(3f, 0f))
-            add(Entry(4f, 14f))
-            add(Entry(5f, 30f))
-            add(Entry(6f, 25f))
-            add(Entry(7f, 50f))
-            add(Entry(8f, 37f))
-            add(Entry(9f, 25f))
-            add(Entry(10f, 51f))
-            add(Entry(11f, 22f))
-            add(Entry(12f, 17f))
-            add(Entry(13f, 16f))
-            add(Entry(14f, 13f))
-            add(Entry(15f, 20f))
-            add(Entry(16f, 34f))
-            add(Entry(17f, 39f))
-            add(Entry(18f, 40f))
-            add(Entry(19f, 35f))
-            add(Entry(20f, 21f))
-            add(Entry(21f, 0f))
-            add(Entry(22f, 5f))
-            add(Entry(23f, 4f))
-            add(Entry(24f, 40f))
-            add(Entry(25f, 31f))
-            add(Entry(26f, 22f))
-            add(Entry(27f, 25f))
-            add(Entry(28f, 47f))
-            add(Entry(29f, 23f))
-            add(Entry(30f, 38f))
-            add(Entry(31f, 24f))
-        }
+        // 서버에서 혐오 표현 사용 횟수 데이터를 가져옴.
+        val mutableList = getData()
 
         val lineDateSet = LineDataSet(mutableList, "혐오 표현 사용 횟수").apply {
             axisDependency = YAxis.AxisDependency.LEFT // Y값 데이터를 왼쪽으로 배치
@@ -125,5 +95,100 @@ class ChartFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initListeners() {
+        binding.button1.setOnClickListener {
+            // TODO
+        }
+
+        binding.button2.setOnClickListener {
+            // TODO
+        }
+
+        binding.button3.setOnClickListener {
+            // TODO
+        }
+
+        binding.button4.setOnClickListener {
+            // TODO
+        }
+
+        binding.button5.setOnClickListener {
+            // TODO
+        }
+
+        binding.button6.setOnClickListener {
+            // TODO
+        }
+
+        binding.button7.setOnClickListener {
+            // TODO
+        }
+
+        binding.button8.setOnClickListener {
+            // TODO
+        }
+
+        binding.button9.setOnClickListener {
+            // TODO
+        }
+
+        binding.button10.setOnClickListener {
+            // TODO
+        }
+
+        binding.button11.setOnClickListener {
+            // TODO
+        }
+
+        binding.button12.setOnClickListener {
+            // TODO
+        }
+    }
+
+    // TODO: 서버에서 혐오 표현 사용 횟수 데이터를 가져옴.
+    private fun getData(): MutableList<Entry> {
+        val accountID = requireActivity()
+            .getSharedPreferences("cbAuto", 0)
+            .getString("id", "")!!
+        
+        // TODO: 서버에서 데이터 가져오기
+
+        val mutableList = mutableListOf<Entry>().apply {
+            add(Entry(1f, 1f))
+            add(Entry(2f, 2f))
+            add(Entry(3f, 0f))
+            add(Entry(4f, 14f))
+            add(Entry(5f, 30f))
+            add(Entry(6f, 25f))
+            add(Entry(7f, 50f))
+            add(Entry(8f, 37f))
+            add(Entry(9f, 25f))
+            add(Entry(10f, 51f))
+            add(Entry(11f, 22f))
+            add(Entry(12f, 17f))
+            add(Entry(13f, 16f))
+            add(Entry(14f, 13f))
+            add(Entry(15f, 20f))
+            add(Entry(16f, 34f))
+            add(Entry(17f, 39f))
+            add(Entry(18f, 40f))
+            add(Entry(19f, 35f))
+            add(Entry(20f, 21f))
+            add(Entry(21f, 0f))
+            add(Entry(22f, 5f))
+            add(Entry(23f, 4f))
+            add(Entry(24f, 40f))
+            add(Entry(25f, 31f))
+            add(Entry(26f, 22f))
+            add(Entry(27f, 25f))
+            add(Entry(28f, 47f))
+            add(Entry(29f, 23f))
+            add(Entry(30f, 38f))
+            add(Entry(31f, 24f))
+        }
+
+        return mutableList
     }
 }
