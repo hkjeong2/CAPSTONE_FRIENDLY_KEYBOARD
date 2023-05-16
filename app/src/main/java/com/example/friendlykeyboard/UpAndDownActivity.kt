ed58fd62b0ae342feb566f93c7a07cab3d4df948
hkjeong2 <chali0330@gmail.com>
2023-05-16 11:58:30 +0900

    upAndDown - chatgpt 응답, UI 수정, numpad 기능 작업 필요

--- app/src/main/java/com/example/friendlykeyboard/UpAndDownActivity.kt
+++ app/src/main/java/com/example/friendlykeyboard/UpAndDownActivity.kt
@@ -3,24 +3,24 @@ package com.example.friendlykeyboard
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.Typeface
+import android.opengl.Visibility
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.StyleSpan
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.friendlykeyboard.databinding.ActivityUpAndDownBinding
 import com.example.friendlykeyboard.retrofit_util.Account
 import com.example.friendlykeyboard.retrofit_util.HateSpeechCountDataModel
-import com.example.friendlykeyboard.retrofit_util.HateSpeechDataModel
 import com.example.friendlykeyboard.retrofit_util.RetrofitClient
 import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class UpAndDownActivity : AppCompatActivity() {
 
@@ -38,19 +38,19 @@ class UpAndDownActivity : AppCompatActivity() {
 
         initSetting()
         initScreen()
-        initClickListener()
         initAnswer()
+        initClickListener()
 
     }
 
     private fun initSetting(){
         spf = getSharedPreferences("setting", 0)
         curse = intent.getStringExtra("curse")!!
-
     }
 
     private fun initScreen(){
-        randomIndex = (list.indices).random()
+//        randomIndex = (list.indices).random()
+        randomIndex = Random().nextInt(list.size)
         val randomWords = list[randomIndex]
         var words = "\"" + randomWords + "\""
         if (randomWords != "악플/욕설"){
@@ -59,31 +59,6 @@ class UpAndDownActivity : AppCompatActivity() {
         val spannableString = SpannableString(words)
         spannableString.setSpan(StyleSpan(Typeface.BOLD), 0, randomWords!!.length + 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         binding.textViewMain.text = spannableString
-    }
-
-    private fun initClickListener(){
-        binding.inputButton.setOnClickListener{
-            initStage()
-            CoroutineScope(Dispatchers.Main).launch{
-                delay(500)
-                Toast.makeText(applicationContext, "미션 성공", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    private fun initStage(){
-        spf.edit().putInt("stageNumber", 0).apply()
-
-        //기능설정 색깔 복구
-        spf.edit().putInt("settingAlarmColor", Color.parseColor("#000000")).apply()
-        spf.edit().putInt("settingInvisibleColor", Color.parseColor("#000000")).apply()
-        spf.edit().putInt("settingEnglishColor", Color.parseColor("#000000")).apply()
-        spf.edit().putInt("settingRandomColor", Color.parseColor("#000000")).apply()
-        spf.edit().putInt("settingCorrectColor", Color.parseColor("#000000")).apply()
-
-        // 키보드 폰트색 복구
-        spf.edit().putInt("keyboardFontColor", spf.getInt("tempKeyboardFontColor", 0)).apply()
     }
 
     private fun initAnswer(){
@@ -117,9 +92,6 @@ class UpAndDownActivity : AppCompatActivity() {
                         sum += counts[key] ?: 0
                     }
                     answer = sum
-
-                    Log.d("result", counts.toString())
-                    Log.d("result", answer.toString())
                 }
                 else{
                     Log.d("UpAndDownActivity", response.message())
@@ -135,5 +107,86 @@ class UpAndDownActivity : AppCompatActivity() {
 
         })
     }
+
+    private fun initClickListener(){
+        // 숫자 입력 시
+        binding.inputButton.setOnClickListener{
+            checkAnswer()
+        }
+        // 정답 처리
+        binding.understandCheckBox.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if (isChecked){
+                binding.understandCheckBox.isClickable = false
+
+                initStage()
+                CoroutineScope(Dispatchers.Main).launch{
+                    delay(500)
+                    Toast.makeText(applicationContext, "미션 성공", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun checkAnswer(){
+        val text = binding.editText.text
+        binding.editText.setText("")
+
+        if (text.toString() == ""){
+            binding.inputNumber.text = "숫자를 입력해주세요!"
+        }
+        else{
+            binding.inputNumber.text = text
+            val inputNumber = text.toString().toInt()
+
+            if (inputNumber > answer){
+                binding.updown.text = "Down"
+                setVisibility(false)
+            }
+            else if (inputNumber < answer){
+                binding.updown.text = "Up"
+                setVisibility(false)
+            }
+            else{
+                binding.updown.text = "정답!"
+                loadResponse()
+                setVisibility(true)
+            }
+        }
+    }
+
+    private fun loadResponse(){
+        //chatGPT 호출
+
+
+    }
+
+    private fun setVisibility(flag : Boolean){
+        if (flag){
+            binding.advice.visibility = View.VISIBLE
+            binding.understand.visibility = View.VISIBLE
+            binding.understandCheckBox.visibility = View.VISIBLE
+        }
+        else {
+            binding.advice.visibility = View.INVISIBLE
+            binding.understand.visibility = View.INVISIBLE
+            binding.understandCheckBox.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun initStage(){
+        spf.edit().putInt("stageNumber", 0).apply()
+
+        //기능설정 색깔 복구
+        spf.edit().putInt("settingAlarmColor", Color.parseColor("#000000")).apply()
+        spf.edit().putInt("settingInvisibleColor", Color.parseColor("#000000")).apply()
+        spf.edit().putInt("settingEnglishColor", Color.parseColor("#000000")).apply()
+        spf.edit().putInt("settingRandomColor", Color.parseColor("#000000")).apply()
+        spf.edit().putInt("settingCorrectColor", Color.parseColor("#000000")).apply()
+
+        // 키보드 폰트색 복구
+        spf.edit().putInt("keyboardFontColor", spf.getInt("tempKeyboardFontColor", 0)).apply()
+    }
+
 
 }
