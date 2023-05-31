d1c2c0b262fefdca0da1087f7da8fee2f417c51e
HyunJinNo <nhj9831@naver.com>
2023-05-31 16:34:37 +0900

    FrameLayout을 ConstraintLayout으로 변경

---
+++ app/src/main/java/com/example/friendlykeyboard/fragments/ViewPagerFragment6.kt
@@ -0,0 +1,27 @@
+package com.example.friendlykeyboard.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.friendlykeyboard.R
+import com.example.friendlykeyboard.databinding.FragmentViewPager6Binding
+
+class ViewPagerFragment6 : Fragment() {
+    private var _binding: FragmentViewPager6Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentViewPager6Binding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
